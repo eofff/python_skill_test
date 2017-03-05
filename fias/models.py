4aad3eb134cccb1c4fa1968236043888c25a8359
fearning <b1kapb999@gmail.com>
2017-03-05 23:25:07 +0300

    add model for addr

--- fias/models.py
+++ fias/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class AddrObj(models.Model):
+    IFNSFL = models.IntegerField()
+    IFNSUL = models.IntegerField()
+    OKATO = models.IntegerField()
+    OKTMO = models.IntegerField()
+    postal_code = models.IntegerField()
+    status = models.BooleanField()
+    update_date = models.DateField()
+    formal_name = models.CharField(max_length=200)
+    official_name = models.CharField(max_length=200)
+    short_name = models.CharField(max_length=20, default='')
