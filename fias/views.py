54c9422c1a039202c6747e50710449d0d2292f97
fearning <b1kapb999@gmail.com>
2017-03-06 00:47:35 +0300

    modify views.py and main template for viewing addr records

--- fias/views.py
+++ fias/views.py
@@ -1,6 +1,14 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.db.models import Q
+from .models import AddrObj
 
 
 def index(request):
-    return HttpResponse(render(request, 'fias/index.html', {}))
+    data = {}
+    if request.POST:
+        addr = request.POST.get('addr', '')
+        data['addr'] = addr
+        response = AddrObj.objects.filter(Q(formal_name__contains=addr) | Q(official_name__contains=addr))
+        data['response'] = response
+    return HttpResponse(render(request, 'fias/index.html', data))
