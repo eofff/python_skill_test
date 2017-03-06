baf00fe77c19c7ef47c9ca65250df27c10cc4068
fearning <b1kapb999@gmail.com>
2017-03-06 12:02:35 +0300

    add api method for autocomplete addr field

--- fias/urls.py
+++ fias/urls.py
@@ -5,4 +5,5 @@ from . import views
 app_name = 'fias'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^api_complete_addr$', views.api_complete_addr, name='api_complete_addr'),
 ]
--- fias/views.py
+++ fias/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render
 from django.db.models import Q
 from .models import AddrObj
 
+import json
+
 
 def index(request):
     data = {}
@@ -12,3 +14,12 @@ def index(request):
         response = AddrObj.objects.filter(Q(formal_name__contains=addr) | Q(official_name__contains=addr))
         data['response'] = response
     return HttpResponse(render(request, 'fias/index.html', data))
+
+
+def api_complete_addr(request):
+    response = {'result': []}
+    if request.POST:
+        addr = request.POST.get('addr', '')
+        if addr != '':
+            response = AddrObj.objects.filter(Q(formal_name__startswith=addr) | Q(official_name__startswith=addr))[:10]
+    return HttpResponse(json.dumps(response))
